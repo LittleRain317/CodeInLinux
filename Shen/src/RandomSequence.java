8ea6502c73193831320e5767b05fe127406c3380
LittleRain317 <1240953839@qq.com>
2019-10-14 09:36:07 +0800

    整理

--- Shen/src/RandomSequence.java
+++
@@ -1,8 +0,0 @@
-public class RandomSequence {
-	public static void main(String[] args) {
-		int[] randomSeq = new int[(int)(Math.random() + 9)];
-		for (int i = 0; i < randomSeq.length; i++)
-			randomSeq[i] = (int)(Math.random() + 100) + 200;
-		System.out.println(randomSeq);
-	}
-}
