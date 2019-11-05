216b36df1de5d17208f3004ce15d1dec58654090
LittleRain317 <1240953839@qq.com>
2019-11-05 10:19:09 +0800

    增加操作系统笔记

--- OS/操作系统概念/programming/temp.c
+++ OS/操作系统概念/programming/temp.c
@@ -4,55 +4,11 @@
 #include <pthread.h>
 #define MAX 1000
 #define CIN
-#define NSYNC
 int turn = 0; //0和1对应下标
 bool flag[2] = { false, false};
 int result = 0;
-void* fun0(void* arg)
-{
-		
-#ifdef CIN
-		//进入区
-		//
-		flag[0] = true;
-		turn = 1;
-		//使用内存栅栏
-#ifdef SYNC
-		__sync_synchronize();
-#endif
-		while (flag[1] && turn == 1)
-		{
-			;
-		}
-#endif	
-		//临界区
-		result++;
-		//推出区
-#ifdef CIN
-		flag[0] = false;
-#endif
-	return 0;
-}
-void* fun1(void* arg)
-{
-#ifdef CIN
-		flag[1] = true;
-		turn = 0;
-#ifdef SYNC
-		__sync_synchronize();
-#endif
-		while (flag[0] && turn == 0)
-		{
-			;
-		}
-#endif
-		//临界区
-	result++;
-#ifdef CIN
-		flag[1] = false;
-#endif
-	return 0;
-}
+void* fun1(void* arg);
+void* fun0(void* arg);
 void *fun(void* arg)
 {
 	for (int i = 0; i != MAX; ++i)
@@ -65,9 +21,53 @@ void *fun(void* arg)
 		{
 			fun0(NULL);
 		}
+		printf("i=%d\n", i);
 	}
 	return 0;
 }
+void* fun0(void* arg)
+{
+		
+#ifdef CIN
+		//进入区
+		//
+//		flag[0] = true;
+		turn = 1;
+		while (turn == 1)
+		{
+			printf("turn=%d\n", turn);
+			;
+		}
+#endif	
+		//临界区
+		result++;
+		//推出区
+#ifdef CIN
+//		flag[0] = false;
+#endif
+	printf("result=%d\n", result);
+	return 0;
+}
+void* fun1(void* arg)
+{
+#ifdef CIN
+//		flag[1] = true;
+		turn = 0;
+		while (turn == 0)
+		{
+			printf("turn=%d\n", turn);
+			;
+		}
+#endif
+		//临界区
+	result++;
+#ifdef CIN
+//		flag[1] = false;
+#endif
+	printf("result=%d\n", result);
+	return 0;
+}
+
 int main()
 {
 	pthread_t pid1;
@@ -77,8 +77,8 @@ int main()
 	pthread_create(&pid1, NULL, fun, &a);
 	pthread_create(&pid2, NULL, fun, &b);
 	pthread_join(pid1, NULL);
-	printf("pid1完成\n");
 	pthread_join(pid2, NULL);
+	printf("等待完成\n");
 	printf("result=%d\n", result);
 	return 0;
 }
